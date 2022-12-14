59e93eb2fffa475c77b3c60285cd9cf39a998392
Ruben <martinillo114@gmail.com>
2022-12-14 04:46:13 +0100

    BC-3 solved problem with catalog refresh

--- book-catalog-client-admin/src/app/header/header.component.ts
+++ book-catalog-client-admin/src/app/header/header.component.ts
@@ -34,4 +34,9 @@ export class HeaderComponent implements OnInit {
     this.router.navigate(['catalog']);
   }
 
+  getCatalog() {
+    this.dataService.findAll();
+    this.router.navigate(['catalog'])
+  }
+
 }
